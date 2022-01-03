56f6b5521d805e1527e55383926e5293392f69dc
Nishant Jain <nishantjainyo@gmail.com>
2022-01-03 21:46:34 +0530

    search in a sorted 2d array

---
+++ DSA Level 1/1. Basic of programming/4. 2D Arrays/10. Search In A Sorted 2d Array/searchInAsortedArray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) 
+{
+    // write your code here
+    int i = 0;
+    int j = matrix[0].size() -1 ;
+
+    while(i<matrix.size() && j>=0){
+        if(matrix[i][j] == target){
+            cout<<i<<"\n"<<j;
+            return true;
+        }else if(matrix[i][j] < target){
+            i++;
+        }else if(matrix[i][j] > target){
+            j--;
+        }
+    }
+
+    return false;
+}
+
+int main() 
+{
+    int n, target;
+    cin>>n;
+    vector<vector<int>> mat(n, vector<int> (n));
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin>>mat[i][j];
+    cin>>target;
+
+    if(!searchMatrix(mat, target)) cout<<"Not Found\n";
+}
