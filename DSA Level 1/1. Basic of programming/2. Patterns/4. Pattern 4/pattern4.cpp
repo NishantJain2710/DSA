b5c883ecce4883b69f245fd29d8e277f8d952c3e
Nishant Jain <nishantjainyo@gmail.com>
2022-01-06 06:00:47 +0530

    Pattern 4

---
+++ DSA Level 1/1. Basic of programming/2. Patterns/4. Pattern 4/pattern4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout<<"\t";
+        }
+        for(int j=n-1; j>=i; j--){
+            cout<<"*\t";
+        }
+        cout<<"\n";
+    }
+
+}
